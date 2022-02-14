ab52a9bd95f5bac6903aa64b31e0af50183a4cb5
sparsh <sparshgr8@yahoo.com>
2022-02-14 10:43:19 +0530

    commented the code

--- src/store.js
+++ src/store.js
@@ -2,12 +2,15 @@ import { createStore } from "redux";
 const SET_SECTION_NAME = 'SET_SECTION_NAME';
 const initalState = 0;
 
+
+// action.js
 export const setSectionName = (sectionName) => ({
     type: SET_SECTION_NAME,
     payload : sectionName
 });
 
 
+// reducer.js
 const reducer = (state = initalState, action) => {
     switch(action.type) {
         case SET_SECTION_NAME : return action.payload;
@@ -15,6 +18,8 @@ const reducer = (state = initalState, action) => {
     }
 }
 
+
+// store.js
 const store = createStore(reducer);
 
 export default store;
